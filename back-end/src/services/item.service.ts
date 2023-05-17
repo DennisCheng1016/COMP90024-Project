978a21b0815c07f002e10e5067abfd5a43aa00a5
zhuocun <zhuocun.tang@outlook.com>
2023-05-17 18:37:55 +1000

    feat: setup real view request

--- back-end/src/services/item.service.ts
+++ back-end/src/services/item.service.ts
@@ -1,13 +1,12 @@
 import { couchDBClient } from "../couchdb";
 
-const MY_VIEW = "items";
 const getItemById = async (itemId: string) => {
     const item = await couchDBClient.get(itemId);
     return item as unknown as IItem;
 }
 
 const getItemsByView = async () => {
-    const items = couchDBClient.view(MY_VIEW, "items")
+    const items = couchDBClient.view("test", "test-view")
     return items as unknown as IItem[];
 }
 

4b7c909cfeac7cfb054eca591f839de190377184
zhuocun <zhuocun.tang@outlook.com>
2023-05-21 18:06:31 +1000

    feat: updated special data handler

--- back-end/src/utils/merge.ts
+++ back-end/src/utils/merge.ts
@@ -14,7 +14,7 @@ const mergeAnalysis = (analysisVic: ILocAnalysis[], analysisGmel: ILocAnalysis[]
 const handleData = (data: ILocAnalysis[]) => {
     const value: number = data.find((el) => el.key === "MELBOURNE")?.value || 0;
     return data
-        .concat([{ key: "YARRA", value }, { key: "PORT PHILLIP", value }, { key: "MARIBYRNONG", value }, { key: "MORELAND", value }, { key: "STONNINGTON", value }, { key: "DAREBIN", value }]);
+        .concat([{ key: "YARRA", value }, { key: "PORT PHILLIP", value }, { key: "MARIBYRNONG", value }, { key: "MORELAND", value }, { key: "STONNINGTON", value }, { key: "DAREBIN", value }, { key: "MOONEE VALLEY", value }, { key: "BANYULE", value }, { key: "BOROONDARA", value }]);
 }
 
 export { mergeAnalysis };

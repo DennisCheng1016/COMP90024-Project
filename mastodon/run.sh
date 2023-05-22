df43bc52f7daeab2668638e98740e58da88f0aa0
Dennis <honor1016dd@gmail.com>
2023-05-23 00:54:20 +1000

    refactor: restructure the mastodon and ansible

--- mastodon/run.sh
+++
@@ -1,4 +0,0 @@
-sudo apt-get update && sudo apt-get install python3 python3-pip ansible
-sudo mkdir /etc/ansible
-echo 'localhost' | sudo tee /etc/ansible/hosts
-sudo ansible-playbook -c local setup-harvest.yaml
